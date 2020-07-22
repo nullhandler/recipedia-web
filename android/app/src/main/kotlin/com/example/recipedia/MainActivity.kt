f68cb31ba7a5ea98e996b52468fc7f6cf0a24bc3
OjasKarmarkar <ojask2002@gmail.com>
2020-07-22 22:04:07 +0530

    fix

--- android/app/src/main/kotlin/com/example/recipedia/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/recipedia/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.recipedia
-
+import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine;
 
 class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
 }
