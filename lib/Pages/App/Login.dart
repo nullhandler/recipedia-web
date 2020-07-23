b8e336aaa5ac32ec22c3f6d787f36a6798569e86
OjasKarmarkar <ojask2002@gmail.com>
2020-07-23 16:17:49 +0530

    Ui addn

--- lib/Pages/App/Login.dart
+++ lib/Pages/App/Login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import '../../keys.dart' as secret;
+import 'dart:ui' as ui;
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,49 +9,72 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  GoogleSignIn _googleSignIn = GoogleSignIn(clientId: secret.clientID );
+  GoogleSignIn _googleSignIn = GoogleSignIn(clientId: secret.clientID);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlutterLogo(size: 150),
-              SizedBox(height: 50),
-              _signInButton(),
-            ],
+      body: Stack(children: <Widget>[
+        Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("assets/background-app.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: BackdropFilter(
+            filter: ui.ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: new RotationTransition(
+                        turns: new AlwaysStoppedAnimation(-10 / 360),
+                        child: Text(
+                          "Recipedia",
+                          style: TextStyle(
+                              fontSize: 60,
+                              color: Colors.white,
+                              fontFamily: 'RobotoMono'),
+                        )),
+                  ),
+                  SizedBox(height: 100),
+                  Align(child: _signInButton()),
+                ],
+              ),
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 
   Widget _signInButton() {
     return OutlineButton(
       splashColor: Colors.grey,
-      onPressed: () {},
+      onPressed: () async {
+        _googleSignIn.signIn();
+      },
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
       highlightElevation: 0,
-      borderSide: BorderSide(color: Colors.grey),
+      borderSide: BorderSide(color: Colors.white),
       child: Padding(
         padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
         child: Row(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Image(image: AssetImage("assets/google_logo.png"), height: 25.0),
             Padding(
               padding: const EdgeInsets.only(left: 10),
               child: Text(
                 'Sign in with Google',
                 style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
+                  fontSize: 18,
+                  color: Colors.white,
                 ),
               ),
             )
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:recipedia/Pages/App/Home.dart';
 import 'package:recipedia/Pages/Website/Home.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'Pages/App/Login.dart';
+import 'keys.dart' as secret;
 
 void main() {
   runApp(MyApp());
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
+    GoogleSignIn _googleSignIn = GoogleSignIn(clientId: secret.clientID);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,6 +27,11 @@ class MyApp extends StatelessWidget {
         home: (kIsWeb)
             ? WebHome()
             // running on the web
-            : AppHome());
+            : LoginScreen());
   }
+
+ void screenToReturn(){
+  print(_googleSignIn.isSignedIn());
+ }
+
 }
